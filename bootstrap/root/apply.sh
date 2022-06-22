32aa466ab4ad2019db0df7a1496d4ab15072580f
F. D <fdesjarl@yahoo.com>
2022-06-22 10:25:08 -0700

    Initial commit.

---
+++ bootstrap/root/apply.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curl -fks --connect-timeout 5 https://git.innersphere.xyz \
+    || extra_args="--values values-seed.yaml"
+
+helm template \
+    --include-crds \
+    --namespace argocd \
+    ${extra_args} \
+    argocd . \
+    | kubectl apply -n argocd -f -
+
